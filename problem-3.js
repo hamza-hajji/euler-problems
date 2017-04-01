5b66d6429da84c97a3ecb8634af74eaf7a3957f4
Hamza Hajji <hmzhjji@gmail.com>
2017-04-01 23:36:47 +0100

    Solve problem 3

---
+++ problem-3.js
@@ -0,0 +1,24 @@
+function isPrime(n) {
+	for (let i = 2; i < n; i++) {
+		if (n % i === 0) return false
+	}
+
+	return true
+}
+
+function primeFactors(limit) {
+	let primes = []
+
+	for (let i = 2; i <= limit; i++) {
+		if (limit % i ===0 && isPrime(i))
+			primes.push(i)
+	}
+
+	return primes
+}
+
+function maxPrimeFactor(limit) {
+	return Math.max.apply(null, primeFactors(limit))
+}
+
+var answer = maxPrimeFactor(600851475143)
